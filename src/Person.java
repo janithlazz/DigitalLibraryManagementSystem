ccfc0c452e90675b1871430fa6c227f56f411aee
Janith-lasindu <janithlasindu15@gmail.com>
2025-04-30 13:32:08 +0530

    add Person.java class

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,60 @@
-public class Person {
+import java.time.LocalDate;
+import java.util.Objects;
+
+abstract class Person {
+    private String id;
+    private String name;
+    private String contactInfo;
+    private LocalDate registrationDate;
+
+    public Person(String id, String name, String contactInfo, LocalDate registrationDate) {
+        this.id = id;
+        this.name = name;
+        this.contactInfo = contactInfo;
+        this.registrationDate = registrationDate;
+    }
+
+    public abstract String getRole();
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public LocalDate getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDate registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", contactInfo='" + contactInfo + '\'' +
+                ", registrationDate=" + registrationDate +
+                '}';
+    }
 }
